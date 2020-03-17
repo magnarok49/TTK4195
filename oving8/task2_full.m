09a5e341dfab7a6530307c09077cb4ad6515a56a
Magne Sirnes <magneas49@hotmail.com>
2020-03-17 16:47:35 +0100

    added results task 2

--- oving8/task2_full.m
+++ oving8/task2_full.m
@@ -1,9 +1,8 @@
 
 % ----- Model definition -----
 
-% This defines expressions using syms for qi and Dqi, stacking the vectors 
-% and using subs(expr, [q1,q2,q3,Dq1,Dq2,Dq3],u) should do the trick.
 run('Mats_and_params_for_Assignment_8')
+close all;
 
 % ----- Initial Conditions -----
 
@@ -12,6 +11,7 @@ Dq0 = zeros(3,1);
 
 % ----- Tuning -----
 
+% PD TUNING
 Kp = [[ 3, 0,  0];...
       [ 0, 6,  0];...
       [ 0, 0,  4]];
@@ -20,9 +20,15 @@ Kd = [[ 4,   0,  0];...
       [ 0,   6,  0];...
       [ 0,   0,  6]];
 
+% INVERSE DYNAMICS TUNING
+w1 = 8; w2 = 0.4; w3 = 10;
+z1 = 1;  z2 = 1;  z3 = 1;
+K0 = diag([ w1^2, w2^2, w3^2]);
+K1 = diag([ 2*w1*z1, 2*w2*z2, 2*w3*z3]);
+
 % ----- Simulation settings -----
 
-controller = 0;
+controller = 0; % 0 for PD, 1 for inv dynamics
 mode = "Accelerator";
 time = 50;
 
@@ -47,7 +53,7 @@ r_ts  = tmp.Values;
 figure;
 for i = 1:3
     subplot(3,1,i);
-    title("q_" + num2str(i) + " vs r_" + num2str(i));
+    title("q_" + num2str(i) + " vs r_" + num2str(i) + " with PD control");
     hold on;
     grid on;
     plot(q_ts.Time, q_ts.Data(:,i),'b');
@@ -56,5 +62,41 @@ for i = 1:3
 end
 
 figure;
-title("Inputs");
+title("Inputs with PD control");
+plot(u_ts);
+
+% ----- Simulation -----
+controller = 1;
+mode = "Accelerator";
+time = "50";
+simout = sim('task2.slx',"SimulationMode", mode, ... 
+                         "StopTime"      , num2str(time));
+
+% ----- Scraping data -----
+
+tmp   = get(simout.logsout, 'q');
+q_ts  = tmp.Values;
+tmp   = get(simout.logsout, 'Dq');
+Dq_ts = tmp.Values;
+tmp   = get(simout.logsout, 'u');
+u_ts  = tmp.Values;
+tmp   = get(simout.logsout, 'r');
+r_ts  = tmp.Values;
+
+% ----- Plotting -----
+
+figure;
+for i = 1:3
+    subplot(3,1,i);
+    title("q_" + num2str(i) + " vs r_" + num2str(i) + ...
+          " with inverse dynamics control");
+    hold on;
+    grid on;
+    plot(q_ts.Time, q_ts.Data(:,i),'b');
+    plot(r_ts.Time, r_ts.Data(:,i),'r--');
+    legend("q_" + num2str(i) , "r_" + num2str(i));
+end
+
+figure;
+title("Inputs with inverse dynamics control");
 plot(u_ts);
